84d8ba35b17046eddb533de92f902e0fb68a7858
Leomar Linhares <leomar_linhares@hotmail.com>
2022-01-17 13:23:05 -0300

    :heavy_check_mark: cumpre o requisito 1

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -6,10 +7,16 @@ app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+const talker = './talker.json';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', (_request, response) => {
+  const data = fs.readFileSync(talker, 'utf-8');
+  response.status(HTTP_OK_STATUS).json(JSON.parse(data));
 });
 
 app.listen(PORT, () => {
