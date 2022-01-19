3cf00cdc8008ae685b71453e148a18cfdb074b59
Leomar Linhares <leomar_linhares@hotmail.com>
2022-01-19 11:41:59 -0300

    :sparkles: cria função responsável por gerar token hexadecimal

---
+++ api/helpers/tokenGenerator.js
@@ -0,0 +1,14 @@
+const CryptoJS = require('crypto-js');
+
+function tokenGenerator(authInfo) {
+  const auth = JSON.stringify(authInfo);
+  const salt = CryptoJS.lib.WordArray.random();
+  return CryptoJS.PBKDF2(auth, salt, { keySize: 2 });
+}
+
+module.exports = tokenGenerator;
+
+// Documentação do CryptoJS nesse site https://cryptojs.gitbook.io/docs/
+// Fazer a chave ter 16 caracteres é responsabilidade do keySize. Aparentemente a
+// numeração interfere na quantidade de caracteres multiplicando por 8.
+// Isso foi descoberto por tentativa e erro.
