9be5b56aa37260d05431ce3c7682af372fdd6738
Leomar Linhares <leomar_linhares@hotmail.com>
2022-01-24 19:06:41 -0300

    :construction: refatora middlewares relacionados a propriedade talk para dispensar o helper validateDate

--- api/middlewares/validateTalk.js
+++ api/middlewares/validateTalk.js
@@ -1,8 +1,6 @@
-const validateDate = require('../helpers/validateDate');
-
-const thereIsTalk = (request, response, next) => {
+const validateTalk = (request, response, next) => {
   const { talk } = request.body;
-  if (!talk || !talk.watchedAt || !talk.rate) {
+  if (!talk || !talk.watchedAt || talk.rate === undefined) {
     return response.status(400).json({
       message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
     });
@@ -11,13 +9,20 @@ const thereIsTalk = (request, response, next) => {
   next();
 };
 
-const validateTalk = (request, response, next) => {
+const validateDate = (request, response, next) => {
   const { talk } = request.body;
-  if (!validateDate(talk.watchedAt)) {
+  const regexDate = /^\d{2}\/\d{2}\/\d{4}$/;
+  if (!regexDate.test(talk.watchedAt)) {
     return response.status(400)
       .json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
   }
-  if (talk.rate > 5 || talk.rate < 1) {
+
+  next();
+};
+
+const validateRate = (request, response, next) => {
+  const { talk } = request.body;
+  if (talk.rate < 1 || talk.rate > 5) {
     return response.status(400)
       .json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
   }
@@ -26,6 +31,7 @@ const validateTalk = (request, response, next) => {
 };
 
 module.exports = {
-  thereIsTalk,
   validateTalk,
+  validateRate,
+  validateDate,
 };
