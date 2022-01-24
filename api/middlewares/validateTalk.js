2ceaf8678fad84081fc8a820f70265246e357fd7
Leomar Linhares <leomar_linhares@hotmail.com>
2022-01-24 12:10:40 -0300

    :bug: repara bug adicionando condição que verifica a existência da chave talk

--- api/middlewares/validateTalk.js
+++ api/middlewares/validateTalk.js
@@ -2,7 +2,7 @@ const validateDate = require('../helpers/validateDate');
 
 const thereIsTalk = (request, response, next) => {
   const { talk } = request.body;
-  if (!talk.watchedAt || !talk.rate) {
+  if (!talk || !talk.watchedAt || !talk.rate) {
     return response.status(400).json({
       message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
     });
