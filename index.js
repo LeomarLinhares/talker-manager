343896175d4a9a9c4c0828a8f9b1e7493890b99f
Leomar Linhares <leomar_linhares@hotmail.com>
2022-01-24 11:34:59 -0300

    :construction: aplica os middlewares criados na rota

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const bodyParser = require('body-parser');
 const tokenGenerator = require('./api/helpers/tokenGenerator');
 const { thereIsAEmail, isValidEmail } = require('./api/middlewares/isValidEmail');
 const { haveSixCharacters, thereIsAPassword } = require('./api/middlewares/isValidPassword');
+const validateToken = require('./api/middlewares/validateToken');
+const { thereIsTalk, validateTalk } = require('./api/middlewares/validateTalk');
+const validateName = require('./api/middlewares/validateName');
+const validateAge = require('./api/middlewares/validateAge');
 
 const app = express();
 app.use(bodyParser.json());
@@ -22,6 +26,21 @@ app.get('/talker', (_request, response) => {
   response.status(HTTP_OK_STATUS).json(data);
 });
 
+app.post('/talker',
+  validateToken,
+  validateName,
+  validateAge,
+  thereIsTalk,
+  validateTalk,
+  (request, response) => {
+  const talkerRequest = request.body;
+  const data = JSON.parse(fs.readFileSync(talker, 'utf-8'));
+  response.status(201).json({
+    id: data.length + 1,
+    ...talkerRequest,
+  });
+});
+
 app.get('/talker/:id', (request, response) => {
   const { id } = request.params;
   const data = JSON.parse(fs.readFileSync(talker, 'utf-8'));
@@ -33,10 +52,14 @@ app.get('/talker/:id', (request, response) => {
   }
 });
 
-app.post('/login', thereIsAEmail, isValidEmail, thereIsAPassword, haveSixCharacters, (req, res) => {
-  const { email, password } = req.body;
-  const token = tokenGenerator(req.body);
-  res.status(200).json({ token, email, password });
+app.post('/login',
+  thereIsAEmail,
+  isValidEmail,
+  thereIsAPassword,
+  haveSixCharacters,
+  (request, response) => {
+  const token = tokenGenerator(request.body);
+  response.status(200).json({ token });
 });
 
 app.listen(PORT, () => {
